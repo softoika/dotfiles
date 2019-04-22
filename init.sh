f5236e0be81b21ba36ddf7e7081b107ca3840942
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-04-22 09:19:52 +0900

    add vscode/settings.json

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash -eu
-
 for file in .??*; do
   [[ $file == ".git" ]] && continue
   [[ $file == ".gitignore" ]] && continue
@@ -7,3 +6,7 @@ for file in .??*; do
   
   ln -fns "${PWD}/${file}" "${HOME}/${file}"
 done
+
+# VSCode
+ln -fns "${PWD}/vscode/settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
+
