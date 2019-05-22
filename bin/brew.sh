8ab0654018d327f088ebd49d769d0763314a92fc
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-05-23 08:58:48 +0900

    update bin/brew.sh

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -eu
+#!/usr/bin/env bash 
 
 BREWLIST_DIR="${HOME}/Repositories/dotfiles"
 BREWLIST_PATH="${BREWLIST_DIR}/.brewlist"
@@ -15,10 +15,16 @@ if [[ $1 = "install" ]]; then
     # brew install .brewlist
     else
         if [[ -f $BREWLIST_PATH ]]; then
-            cat $BREWLIST_PATH | while read pkg
+            diff <(brew list) $BREWLIST_PATH > .brewlist.diff
+            cat .brewlist.diff \
+                | grep ">" \
+                | awk '{ print $2 }' \
+                | while read pkg;
             do
+                echo "Found lacking package $pkg";
                 brew install $pkg
             done
+            rm .brewlist.diff
         else
             echo Not found .brewlist
             echo Creating .brewlist ...
