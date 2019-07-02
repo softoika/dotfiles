59c054b7ab932a1002cbc481fc7180889a0f7635
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-07-02 14:26:51 +0900

    Update hub_pull-request.sh

--- bin/hub_pull-request.sh
+++ bin/hub_pull-request.sh
@@ -4,9 +4,8 @@ CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BASE_BRANCH=$(git show-branch | grep '*' | grep -v "${CURRENT_BRANCH}" | head -n1 | awk -F'[]~^[]' '{print $2}')
 PR_LIST=$(hub pr list -f '%i (%H)%n')
 RELEASE_PR=$(printf "${PR_LIST}" | grep $BASE_BRANCH)
-ISSUES=$(echo $CURRENT_BRANCH | gsed -r "s/[^0-9]/\n/g" | egrep "[0-9]")
-TITLE=$(echo $ISSUES | while read line; do echo -n "#${line} "; done)
-DESCRIPTION="## Issues\n$(echo $ISSUES | while read line; do echo "#${line}"; done)\n\n## Summary\n\n## Release PR\n${RELEASE_PR}\n"
-MESSAGE=$(printf "${TITLE}\n\n${DESCRIPTION}")
+ISSUES=$(echo $CURRENT_BRANCH | gsed -r "s/[^0-9]/\n/g" | egrep "[0-9]" | gsed -r "s/([0-9]+)/#\1/g")
+DESCRIPTION="## Issues\n${ISSUES}\n\n## Summary\n\n## Release PR\n${RELEASE_PR}\n"
+MESSAGE=$(printf "$(echo $ISSUES | while read line; do echo -n $line; done)\n\n${DESCRIPTION}")
 hub pull-request -eop -b $BASE_BRANCH -m "${MESSAGE}"
 
