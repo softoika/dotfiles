7fcc9190e30e2cc95440f21dcb9f7c1489ad0e89
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2019-09-01 23:41:14 +0900

    Fix indentation

--- bin/brew.sh
+++ bin/brew.sh
@@ -9,35 +9,35 @@ PACKAGES=${@:2:($#-1)}
 brew upgrade
 
 if [[ $1 = "install" ]]; then
-    # brew install $PACKAGES
-    if [[ $# -gt 1 ]]; then
-        brew install $PACKAGES
-        echo Updating .brewlist ...
-        brew list > $BREWLIST_PATH
-    # brew install .brewlist
-    else
-        if [[ -f $BREWLIST_PATH ]]; then
-            diff <(brew list) $BREWLIST_PATH > .brewlist.diff
-            cat .brewlist.diff \
-                | grep ">" \
-                | awk '{ print $2 }' \
-                | while read pkg;
-            do
-                echo "Found lacking package $pkg";
-                brew install $pkg
-            done
-            rm .brewlist.diff
-        else
-            echo Not found .brewlist
-            echo Creating .brewlist ...
-            brew list > $BREWLIST_PATH
-        fi
-    fi
-elif [[ $1 = "uninstall" ]]; then
-    brew uninstall $PACKAGES
+  # brew install $PACKAGES
+  if [[ $# -gt 1 ]]; then
+    brew install $PACKAGES
     echo Updating .brewlist ...
     brew list > $BREWLIST_PATH
+  # brew install .brewlist
+  else
+    if [[ -f $BREWLIST_PATH ]]; then
+      diff <(brew list) $BREWLIST_PATH > .brewlist.diff
+      cat .brewlist.diff \
+        | grep ">" \
+        | awk '{ print $2 }' \
+        | while read pkg;
+      do
+        echo "Found lacking package $pkg";
+        brew install $pkg
+      done
+      rm .brewlist.diff
+    else
+      echo Not found .brewlist
+      echo Creating .brewlist ...
+      brew list > $BREWLIST_PATH
+    fi
+  fi
+elif [[ $1 = "uninstall" ]]; then
+  brew uninstall $PACKAGES
+  echo Updating .brewlist ...
+  brew list > $BREWLIST_PATH
 else
-    brew $@
+  brew $@
 fi
 
