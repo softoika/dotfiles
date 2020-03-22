2894ea2a6b788a81429a5d724fccd1aa21b8cc9e
r-hanafusa <r_hanafusa@dreamarts.co.jp>
2020-03-22 10:19:27 +0900

    Add .config and its symbolic link script

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@ for file in .??*; do
   [[ $file == ".git" ]] && continue
   [[ $file == ".gitignore" ]] && continue
   [[ $file == ".DS_Store" ]] && continue
+  [[ $file == ".config" ]] && continue
   
   ln -fns "${PWD}/${file}" "${HOME}/${file}"
 done
@@ -14,3 +15,24 @@ ln -fns "${PWD}/vscode/keybindings.json" "${HOME}/Library/Application Support/Co
 # Shell Scripts
 ln -fns "${PWD}/bin" "${HOME}/.local/bin"
 
+# .config
+if [[ ! -e "${HOME}/.config" ]]; then
+  mkdir "${HOME}/.config"
+fi
+for directory in $(find .config -mindepth 1 -maxdepth 1 -type d); do
+  if [[ -e "${HOME}/${directory}" ]]; then
+    read -p "${directory} has already exist. Replace it? (Y/n)" yn
+    case $yn in
+      [Y]*)
+        rm -rf ${HOME}/${directory}
+        echo $directory was removed
+        ;;
+      *)
+        echo $directory was skipped
+        break
+        ;;
+    esac
+  fi
+  echo Create $directory symbolic link
+  ln -fns "${PWD}/${directory}" "${HOME}/.config"
+done
